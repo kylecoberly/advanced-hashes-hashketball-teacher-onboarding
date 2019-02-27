f5ae06df6e6e7ca47ed8a4273d2b76c2807aa25a
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-27 22:55:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,8 +134,7 @@ def good_practices
 end
 
 def num_points_scored player_name
-  game = game_hash
-  game['away']['players'].concat(game_hash['home']['players'])
+  game_hash[:away][:players].concat(game_hash[:home][:players])
 
   binding.pry
 end

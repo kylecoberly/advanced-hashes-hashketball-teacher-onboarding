b729819b39b063f5d29884e594c45df50234498d
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-27 22:04:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,14 +1,25 @@
 def game_hash
   {
     home: {
-      team_name: '',
-      colors: [],
+      team_name: 'Brooklyn Nets',
+      colors: ['Black', 'White'],
       players: {}
     },
     away: {
       team_name: '',
       colors: [],
-      players: {}
+      players: {
+        :'Alan Anderson': {
+          number: '0',
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        }
+      }
     }
   }
 end

d1736b2b1e2ff4b9cc218c97ca0a08271ac74770
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-27 23:06:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -135,8 +135,8 @@ end
 
 def num_points_scored player_name
   players = game_hash[:away][:players].merge(game_hash[:home][:players])
-  player = players[player_name.to_sym]
-  puts player_name.to_sym
+  player = players[player_name]
+  puts player
 end
 
 num_points_scored 'Ben Gordon'
